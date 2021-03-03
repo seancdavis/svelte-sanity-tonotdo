4d5b4dd03c0ecf4a325c047b462a7390f76b9036
Sean C Davis <scdavis41@gmail.com>
2021-03-03 09:03:33 -0500

    Add loading spinner while fetching inactions

--- svelte/src/components/InactionList.svelte
+++ svelte/src/components/InactionList.svelte
@@ -1,11 +1,25 @@
 <script>
   import Inaction from "./Inaction.svelte";
+  import Loader from "./Loader.svelte";
 
   export let inactions = [];
 </script>
 
 <div>
-  {#each inactions as inaction}
-    <Inaction {...inaction} />
-  {/each}
+  {#if inactions.length > 0}
+    {#each inactions as inaction}
+      <Inaction {...inaction} />
+    {/each}
+  {:else}
+    <div class="loading">
+      <Loader />
+    </div>
+  {/if}
 </div>
+
+<style>
+  .loading {
+    margin: 2rem auto;
+    text-align: center;
+  }
+</style>
